6cccc723496f99863ea08af39d3d6df04a0fce70
garethdandrews <garethdandrews@gmail.com>
2020-03-30 17:39:45 +0100

    gets recipes from multiple pages of a category

--- web-scraper.py
+++ web-scraper.py
@@ -2,12 +2,17 @@ from bs4 import BeautifulSoup
 import requests
 import time
 
+root_url = "https://www.bbcgoodfood.com"
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'
 headers = {'User-Agent': user_agent}
 
-def get_recipe_json(url, category):
+def get_content_from_url(url):
     response = requests.get(url, headers=headers, timeout=100)
-    content = BeautifulSoup(response.content, 'html.parser')
+    return BeautifulSoup(response.content, 'html.parser')
+
+
+def get_recipe_json(url, category):
+    content = get_content_from_url(url)
 
     title = content.find('h1', attrs={'class': 'recipe-header__title'}).text
 
@@ -41,7 +46,8 @@ def get_recipe_json(url, category):
         'difficulty': difficulty,
         'serves': serves,
         'ingredients': ingredients_array,
-        'method': method_array
+        'method': method_array,
+        'url': url
     }
 
     return recipe
@@ -56,27 +62,38 @@ def get_list(content):
         i = item.find('a')
         l.append({
             'name': i.text.strip(),
-            'url': "https://www.bbcgoodfood.com" + i['href']
+            'url': root_url + i['href']
         })
     return l
 
 
 def get_category_list(url):
-    response = requests.get(url, headers=headers, timeout=100)
-    content = BeautifulSoup(response.content, 'html.parser')
+    content = get_content_from_url(url)
 
     categories = content.findAll('h3', attrs={'class': 'category-item--title'})
     return get_list(categories)
 
 
+def get_recipes_from_content(content):
+    return get_list(content.findAll('h3', attrs={'class': 'teaser-item__title'}))
+
+
+
 def get_recipe_list(category_list):
-    list = []
+    recipes = []
     for item in category_list:
-        response = requests.get(item['url'], headers=headers, timeout=100)
-        content = BeautifulSoup(response.content, 'html.parser')
-        
-        recipes = content.findAll('h3', attrs={'class': 'teaser-item__title'})
-        get_list(recipes)
+        content = get_content_from_url(item['url'])
+        recipes.append(get_recipes_from_content(content))
+        time.sleep(1)   
+
+        pages = content.findAll('li', attrs={'class': 'pager-item'})
+        for page in pages[1:]:
+            url = root_url + page.find('a')['href']
+            content = get_content_from_url(url)
+            recipes.append(get_recipes_from_content(content))
+            time.sleep(1)
+
+    print(recipes)
 
 
 
@@ -84,7 +101,7 @@ url = "https://www.bbcgoodfood.com/recipes/category/dishes"
 # category_list = get_category_list(url)
 # time.sleep(1)
 
-category_list = [{'name': 'Baked potato', 'url': 'https://www.bbcgoodfood.com/recipes/collection/baked-potato'}]
+category_list = [{'name': 'Curry', 'url': 'https://www.bbcgoodfood.com/recipes/collection/curry'}]
 get_recipe_list(category_list)
 
 
