187b8b85d3f3e1317e9d0f0d89cb49a8058dc046
garethdandrews <garethdandrews@gmail.com>
2020-03-31 10:24:19 +0100

    changed file name to test couchbase

--- database.py
+++ database.py
@@ -3,7 +3,9 @@ from couchbase.cluster import PasswordAuthenticator
 import json
 
 cluster = Cluster('couchbase://localhost')
-
 with open('credentials.json') as json_file:
     data = json.load(json_file)
 authenticator = PasswordAuthenticator(data['username'], data['password'])
+cluster.authenticate(authenticator)
+
+cb = cluster.open_bucket('recipes')
