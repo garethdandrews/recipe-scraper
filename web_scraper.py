ac6029f250ca28f38ec87eb12d479def6787474d
gda <garethdandrews@gmail.com>
2020-11-01 14:05:06 +0000

    changed def

--- web_scraper.py
+++ web_scraper.py
@@ -2,10 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-root_url = "https://www.bbcgoodfood.com/"
 
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'
 headers = {'User-Agent': user_agent}
+
+
+def get_content_from_url(url):
+    response = requests.get(url, headers=headers, timeout=100)
+    return BeautifulSoup(response.content, 'html.parser')
+
 
 def get_recipe(url):
     soup = get_content_from_url(url)
@@ -51,10 +56,5 @@ def get_recipe(url):
 
     recipe['method'] = method
     
-
-def __get_content_from_url(url):
-    response = requests.get(url, headers=headers, timeout=100)
-    return BeautifulSoup(response.content, 'html.parser')
-
 
 get_recipe('https://www.bbcgoodfood.com/recipes/old-delhi-style-butter-chicken')
