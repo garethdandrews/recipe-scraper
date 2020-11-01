24088e7659e9f1d47eefd71a8511c2406f788f27
gda <garethdandrews@gmail.com>
2020-11-01 13:32:49 +0000

    get ingredients

--- web_scraper.py
+++ web_scraper.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 root_url = "https://www.bbcgoodfood.com/"
 
@@ -16,10 +17,10 @@ def get_recipe(url):
 
     title = content.find('h1').text
 
-    times = content.findAll('time')
-
     # metadata (times, difficulty, servings)
     metadata = {}
+
+    times = content.findAll('time')
     for item in times:
         tmp = item.parent.previous_sibling.text[:-1]
         metadata[tmp] = item.text
@@ -27,14 +28,31 @@ def get_recipe(url):
     metadata['difficulty'] = content.find(attrs={'class': 'masthead__skill-level'}).get_text()
     metadata['servings'] = content.find(attrs={'class': 'masthead__servings'}).get_text()
 
+    # ingredients
+    ingredientList = content.find('h2', text=re.compile("^ingredients$", re.I)).parent.findAll('section')
+
+    ingredients = {}
+    for section in ingredientList:
+        heading = section.find('h3')
+        heading = 'main' if heading is None else heading.text
+
+        tmpList = []
+        for item in section.findAll('li'):
+            tmpList.append(item.get_text())
+
+        ingredients[heading] = tmpList
+
+    # compile recipe object
     recipe = {
         'title': title,
-        'metadata': metadata
+        'metadata': metadata,
+        'ingredients': ingredients
     }
 
     print(recipe)
 
+
 get_recipe('https://www.bbcgoodfood.com/recipes/old-delhi-style-butter-chicken')
 
 
-# def scrape_categories(url):
+
