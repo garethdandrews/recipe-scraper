86667878f048de5c124f168c77899899405c6a66
gda <garethdandrews@gmail.com>
2020-11-07 16:37:02 +0000

    checks if a url starts with the root url

--- web_scraper.py
+++ web_scraper.py
@@ -14,6 +14,8 @@ headers = {'User-Agent': user_agent}
 
 # uses the BeautifulSoup4 package to get the content of a webpage
 def get_content_from_url(url):
+    if root_url not in url:
+        url = root_url + url
     response = requests.get(url, headers=headers, timeout=100)
     time.sleep(1)
     return BeautifulSoup(response.content, 'html.parser')
