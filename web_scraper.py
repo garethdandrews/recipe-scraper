36f16b003868b820a18f86e183c52d132bfb903f
gda <garethdandrews@gmail.com>
2020-11-07 16:31:56 +0000

    slightly restructured code to make it recursive

--- web_scraper.py
+++ web_scraper.py
@@ -4,50 +4,34 @@ from bs4 import BeautifulSoup
 import re
 
 import bbcgoodfood
+import recipe_repo
 
 root_url = 'https://www.bbcgoodfood.com'
 
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'
 headers = {'User-Agent': user_agent}
 
+
+# uses the BeautifulSoup4 package to get the content of a webpage
 def get_content_from_url(url):
     response = requests.get(url, headers=headers, timeout=100)
     time.sleep(1)
     return BeautifulSoup(response.content, 'html.parser')
 
 
-# gets a list of category urls from the recipes dropdown box
-def get_category_urls_from_dropdown():
-    soup = get_content_from_url(root_url)
-    recipes_soup = soup.find('span', attrs={'class': 'main-nav__nav-text'}, text=re.compile("^recipes$", re.I)).parent.next_sibling
-    categories_soup = recipes_soup.findChildren('span', text='see more...')
-    return [root_url + category_soup.parent.get('href') for category_soup in categories_soup]
+# gets a list of urls from the headings of a page
+def get_heading_urls(soup):
+    return [title_soup.get('href') for title_soup in soup.findAll('a', attrs={'class': 'standard-card-new__article-title'})]
 
 
-# gets a list of collections from a category - 'collections' hold the list of recipes on bbcgoodfood
-def get_collections_from_category(category_url):
-    soup = get_content_from_url(category_url)
-    pages = [category_url] + get_pagination_urls(soup)
-
-    collection_urls = []
-    for url in pages:
-        soup = get_content_from_url(url)
-        collection_urls += [category_heading.find('a').get('href') for category_heading in soup.findAll('h4')]
-
-    return collection_urls
-
-
-# gets a list of recipes from a collection
-def get_recipes_from_collection(collection_url):
-    soup = get_content_from_url(collection_url)
-    pages = [collection_url] + get_pagination_urls(soup)
-
-    recipe_urls = []
-    for url in pages:
-        soup = get_content_from_url(url)
-        recipe_urls += [a_soup.get('href') for a_soup in soup.findAll('a', attrs={'class': 'standard-card-new__article-title'})]
-
-    return recipe_urls
+# gets a list of urls from the headings of all pages in a section
+def get_headings_from_section(url):
+    soup = get_content_from_url(url)
+    pages_soup = [soup] + [get_content_from_url(page_url) for page_url in get_pagination_urls(soup)]
+    heading_urls = []
+    for page_soup in pages_soup:
+        heading_urls += get_heading_urls(page_soup)
+    return heading_urls
 
 
 # gets a list of urls for the other pages in a category/collection that aren't currently selected 
@@ -57,28 +41,21 @@ def get_pagination_urls(soup):
     if pagination_items is None or len(pagination_items) is 1:
         return []
     pagination_items = pagination_items.findAll('a', attrs={'class': 'pagination-item'})
-    return [root_url + item.get('href') for item in pagination_items]
+    return [item.get('href') for item in pagination_items]
 
 
+# returns true if the url points to a category or a collection
 def is_url_category_or_collection(url):
-    return re.search("(?<=recipes\/)(category|collection)", url).group()
+    return re.search("(?<=recipes\/)(category|collection)", url) is not None
     
 
+def process_url(url):
+    if is_url_category_or_collection(url):
+        [process_url(heading_url) for heading_url in get_headings_from_section(url)]
+    else:
+        soup = get_content_from_url(url)
+        recipe = bbcgoodfood.get_recipe(soup)
+        recipe_repo.insert_one(recipe)
 
-# get a list of categories from the 'recipes' dropdown
-# category_urls = get_category_urls_from_dropdown()
 
-# check if the url is a category or a collection
-
-# go into each collection blah blah blah
-
-
-# for category_url in category_urls:
-    
-
-# # go into each collection
-# for url in collection_urls:
-#     soup = get_content_from_url(url)
-#     recipe = bbcgoodfood.get_recipe(soup)
-
-# persist to database
+process_url('https://www.bbcgoodfood.com/recipes/category')
