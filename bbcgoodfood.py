b8042c8faad73d5af16bcb74a50b275d7896acca
gda <garethdandrews@gmail.com>
2020-11-08 15:14:50 +0000

    remove circular dependency

--- bbcgoodfood.py
+++ bbcgoodfood.py
@@ -1,24 +1,9 @@
-# from web_scraper import get_content_from_url
+from web_scraper import get_content_from_url
 import requests
 import time
 from bs4 import BeautifulSoup
 import re
 import recipe_repo
-
-
-root_url = 'https://www.bbcgoodfood.com'
-
-user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'
-headers = {'User-Agent': user_agent}
-
-
-# uses the BeautifulSoup4 package to get the content of a webpage
-def get_content_from_url(url):
-    if root_url not in url:
-        url = root_url + url
-    response = requests.get(url, headers=headers, timeout=100)
-    time.sleep(1)
-    return BeautifulSoup(response.content, 'html.parser')
 
 
 # iterates through the categories: if a url is a category/collection, it gets the headings from those sections and checks again, until it finds a recipe
@@ -149,4 +134,4 @@ def get_recipe(soup):
     return recipe
 
 
-    
+process_url_for_tags('https://www.bbcgoodfood.com/recipes/category')
--- web_scraper.py
+++ web_scraper.py
@@ -1,10 +1,17 @@
-import bbcgoodfood
+import requests
+import time
+from bs4 import BeautifulSoup
 
-# starts the scraper to add recipes to the database
-def start():
-    bbcgoodfood.process_url('https://www.bbcgoodfood.com/recipes/category')
+root_url = 'https://www.bbcgoodfood.com'
+
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'
+headers = {'User-Agent': user_agent}
 
 
-# starts the scraper to add tags to each recipe in the database
-def start_tag_search():
-    bbcgoodfood.process_url_for_tags('https://www.bbcgoodfood.com/recipes/category')
+# uses the BeautifulSoup4 package to get the content of a webpage
+def get_content_from_url(url):
+    if root_url not in url:
+        url = root_url + url
+    response = requests.get(url, headers=headers, timeout=100)
+    time.sleep(1)
+    return BeautifulSoup(response.content, 'html.parser')
