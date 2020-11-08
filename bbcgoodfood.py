902c6f7a2bea955a3ebf728b5c067e2f2522ef6a
gda <garethdandrews@gmail.com>
2020-11-08 12:04:52 +0000

    reordered functions

--- bbcgoodfood.py
+++ bbcgoodfood.py
@@ -3,36 +3,14 @@ from bs4 import BeautifulSoup
 import re
 import recipe_repo
 
+# if a url is a category/collection, it gets the headings from those sections and checks again, until it finds a recipe
+def process_url(url):
+    print("Processing url: {0}".format(url))
+    if is_url_category_or_collection(url):
+        [process_url(heading_url) for heading_url in get_headings_from_section(url)]
+    else:
+        process_recipe(url)
 
-# gets a list of urls from the headings of a page
-def get_heading_urls(soup):
-    return [title_soup.get('href') for title_soup in soup.findAll('a', attrs={'class': 'standard-card-new__article-title'})]
-
-
-# gets a list of urls from the headings of all pages in a section
-def get_headings_from_section(url):
-    soup = get_content_from_url(url)
-    pages_soup = [soup] + [get_content_from_url(page_url) for page_url in get_pagination_urls(soup)]
-    heading_urls = []
-    for page_soup in pages_soup:
-        heading_urls += get_heading_urls(page_soup)
-    return heading_urls
-
-
-# gets a list of urls for the other pages in a category/collection that aren't currently selected 
-# e.g. if on page 1 of 3, it will get the urls of pages 2 and 3
-def get_pagination_urls(soup):
-    pagination_items = soup.find('div', attrs={'pagination'})
-    if pagination_items is None or len(pagination_items) is 1:
-        return []
-    pagination_items = pagination_items.findAll('a', attrs={'class': 'pagination-item'})
-    return [item.get('href') for item in pagination_items]
-
-
-# returns true if the url points to a category or a collection, but false if it is a recipe
-def is_url_category_or_collection(url):
-    return re.search("(?<=recipes\/)(category|collection)", url) is not None
-    
 
 # scrapes the recipe data and adds it to the database, if its not already in there
 def process_recipe(url):
@@ -53,14 +31,35 @@ def process_recipe(url):
         print("Added recipe: {0}".format(recipe['title']))
 
 
-# if a url is a category/collection, it gets the headings from those sections and checks again, until it finds a recipe
-def process_url(url):
-    print("Processing url: {0}".format(url))
-    if is_url_category_or_collection(url):
-        [process_url(heading_url) for heading_url in get_headings_from_section(url)]
-    else:
-        process_recipe(url)
+# gets a list of urls from the headings of all pages in a section
+def get_headings_from_section(url):
+    soup = get_content_from_url(url)
+    pages_soup = [soup] + [get_content_from_url(page_url) for page_url in get_pagination_urls(soup)]
+    heading_urls = []
+    for page_soup in pages_soup:
+        heading_urls += get_heading_urls(page_soup)
+    return heading_urls
 
+
+# gets a list of urls from the headings of a page
+def get_heading_urls(soup):
+    return [title_soup.get('href') for title_soup in soup.findAll('a', attrs={'class': 'standard-card-new__article-title'})]
+
+
+# gets a list of urls for the other pages in a category/collection that aren't currently selected 
+# e.g. if on page 1 of 3, it will get the urls of pages 2 and 3
+def get_pagination_urls(soup):
+    pagination_items = soup.find('div', attrs={'pagination'})
+    if pagination_items is None or len(pagination_items) is 1:
+        return []
+    pagination_items = pagination_items.findAll('a', attrs={'class': 'pagination-item'})
+    return [item.get('href') for item in pagination_items]
+
+
+# returns true if the url points to a category or a collection, but false if it is a recipe
+def is_url_category_or_collection(url):
+    return re.search("(?<=recipes\/)(category|collection)", url) is not None
+    
 
 def get_recipe(soup):
     def get_text_if_not_none(class_attrs):
