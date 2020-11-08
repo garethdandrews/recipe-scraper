ca89afa810b94e6e206db1affb4edca1f8627943
gda <garethdandrews@gmail.com>
2020-11-08 12:03:45 +0000

    tidied up repository

--- web_scraper.py
+++ web_scraper.py
@@ -2,9 +2,7 @@ import requests
 import time
 from bs4 import BeautifulSoup
 import re
-
 import bbcgoodfood
-import recipe_repo
 
 root_url = 'https://www.bbcgoodfood.com'
 
@@ -21,64 +19,6 @@ def get_content_from_url(url):
     return BeautifulSoup(response.content, 'html.parser')
 
 
-# gets a list of urls from the headings of a page
-def get_heading_urls(soup):
-    return [title_soup.get('href') for title_soup in soup.findAll('a', attrs={'class': 'standard-card-new__article-title'})]
-
-
-# gets a list of urls from the headings of all pages in a section
-def get_headings_from_section(url):
-    soup = get_content_from_url(url)
-    pages_soup = [soup] + [get_content_from_url(page_url) for page_url in get_pagination_urls(soup)]
-    heading_urls = []
-    for page_soup in pages_soup:
-        heading_urls += get_heading_urls(page_soup)
-    return heading_urls
-
-
-# gets a list of urls for the other pages in a category/collection that aren't currently selected 
-# e.g. if on page 1 of 3, it will get the urls of pages 2 and 3
-def get_pagination_urls(soup):
-    pagination_items = soup.find('div', attrs={'pagination'})
-    if pagination_items is None or len(pagination_items) is 1:
-        return []
-    pagination_items = pagination_items.findAll('a', attrs={'class': 'pagination-item'})
-    return [item.get('href') for item in pagination_items]
-
-
-# returns true if the url points to a category or a collection, but false if it is a recipe
-def is_url_category_or_collection(url):
-    return re.search("(?<=recipes\/)(category|collection)", url) is not None
-    
-
-# scrapes the recipe data and adds it to the database, if its not already in there
-def process_recipe(url):
-    if recipe_repo.is_recipe_in_db(url):
-        print("Recipe already in DB: {0}".format(url))
-        return
-    
-    soup = get_content_from_url(url)
-    recipe = None
-    try:
-        recipe = bbcgoodfood.get_recipe(soup)
-    except:
-        print("Skipped recipe with url: {0}".format(url))
-    
-    if recipe is not None:
-        recipe['url'] = url
-        recipe_repo.insert_one(recipe)
-        print("Added recipe: {0}".format(recipe['title']))
-
-
-# if a url is a category/collection, it gets the headings from those sections and checks again, until it finds a recipe
-def process_url(url):
-    print("Processing url: {0}".format(url))
-    if is_url_category_or_collection(url):
-        [process_url(heading_url) for heading_url in get_headings_from_section(url)]
-    else:
-        process_recipe(url)
-
-
 # starts the scraper
 def start():
-    process_url('https://www.bbcgoodfood.com/recipes/category')
+    bbcgoodfood.process_url('https://www.bbcgoodfood.com/recipes/category')
