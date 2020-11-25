ddcc3604c3abe415aa75ea5ecfb6d1ee337adfca
gda <garethdandrews@gmail.com>
2020-11-25 19:11:58 +0000

    testing spacy

--- spacy_test.py
+++ spacy_test.py
@@ -1,5 +1,12 @@
+from pprint import pprint
 import spacy
 from spacy import displacy
 from collections import Counter
 import en_core_web_sm
 nlp = en_core_web_sm.load()
+
+src = "4-5 garlic cloves, crushed"
+
+doc = nlp(src)
+pprint([(X.text, X.label_) for X in doc.ents])
+pprint([(X, X.ent_iob_, X.ent_type_) for X in doc])
